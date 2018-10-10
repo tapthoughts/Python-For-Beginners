1fba2642d9e2edf9e9375027c68cc5f7399b4458
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-10 12:17:04 +0530

    Add files via upload

---
+++ 003-Variables-And-Datatypes.py
@@ -0,0 +1,21 @@
+# Introducing variables
+character_name = "Shivam"
+character_age = "19.1234"
+character_location = "Delhi"
+character_college = "HMRITM"
+
+print("Hi there, I am " + character_name + ".")
+print("")
+print(character_name + " is " + character_age + " years old.")
+print("")
+
+# We can change the values of variables in between simply by introducing variables again
+# Remember max length of a comment is 120 words
+
+character_name = "Abc"
+
+print(character_name + " lives in " + character_location + "")
+print("")
+print(character_name + " studies in " + character_college + "")
+
+
