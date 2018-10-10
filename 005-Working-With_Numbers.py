877612e5032f442efed7da0103fa143b64eb296b
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-10 23:37:25 +0530

    Add files via upload

---
+++ 005-Working-With_Numbers.py
@@ -0,0 +1,55 @@
+# Working with numbers in python couldn't be easier
+print(2)
+print(3.4335)
+print(-2.343)
+print("")
+
+
+# Python follows BODMAS
+print(3 + 4.5 - 3 * 2 / 2 % 2)
+print(3 * (3 + 2) / 2 + (2 * 3) % 2)
+print("")
+
+
+# Variables can also be made for storing numbers
+abc = 2.33
+print(abc)
+print("")
+
+
+# Numbers can also be stored as string
+abc = 5
+print(str(abc) + " is a prime no.")
+print("")
+
+
+# For absolute value of a no.
+abc = -5
+print(abs(abc))
+print("")
+
+
+# Power Function
+print(pow(351, 244))
+print("")
+
+
+# To determine which no. is higher or lower or rounding nos.
+print(min(3, 4, 5, 8))
+print(max(4, 6, 8, 9, 111, 1213))
+print(round(3.4343434))
+print(round(3.6676766767))
+print("")
+
+
+# For more functions we need to import math lib
+from math import *
+
+#Floor function always gives the lower value and ceil always gives higher value
+print(floor(3.9))
+print(ceil(3.1))
+print("")
+
+
+# For square root
+print(sqrt(3.4))
