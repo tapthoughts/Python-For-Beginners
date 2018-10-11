24dddf36fdefc7219f50f4dcc1158732fc411ce5
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-11 17:52:13 +0530

    Add files via upload

---
+++ 008-Mad-Libs-Game.py
@@ -0,0 +1,9 @@
+colour = input("name a color:")
+flower = input("name of a flower in plural:")
+number = input("mention a no.:")
+shape = input("mention a shape:")
+print("")
+
+
+print(flower + " are " + colour)
+print("there are " + number + " of corners in " + shape)
