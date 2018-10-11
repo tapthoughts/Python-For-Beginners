45fa07aba02766617e3712eec6537f9158d58f95
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-11 18:56:31 +0530

    Add files via upload

---
+++ 009-Lists.py
@@ -0,0 +1,25 @@
+abc = ["Nik", "Sam", "Shivam", 2, 4, False]
+# 2, ,4, False etc can be used even without ("")
+print(abc)
+print("")
+
+# To print a specific element out of an array
+print(abc[1])
+print(abc[4])
+print(abc[0])
+print("")
+
+# Negative values in arrays starts giving values from back end
+# Although the listing of elements starts by 0 in an array
+# From backwards the listing starts from 1
+print(abc[-2])
+print(abc[-4])
+print(abc[-1])
+print("")
+
+# For changing values in an array
+abc = ["Nik", "Sam", "Shivam", 2, 4, False]
+print(abc[2 ])
+abc[2] = "Raman"
+print(abc[2])
+
