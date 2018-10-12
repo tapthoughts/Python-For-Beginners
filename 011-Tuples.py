612e623c70d6db10b46d66bc48d08aac103dfab5
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-12 13:33:24 +0530

    Add files via upload

---
+++ 011-Tuples.py
@@ -0,0 +1,13 @@
+# Tuples can't be changed or modified once created
+# Tuples are generally used for data that will not be needing any changes
+coordinates = (6, 5, 7, 9, 8)
+print(coordinates[3])
+print(coordinates)
+print("")
+
+
+# Whereas arrays can be always changed though
+abc = [4, 6, 7, 8, 9]
+abc[0] = 2
+print(abc)
+
