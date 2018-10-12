5afa6eb910c938eb03d5b8121fef1ff2a9b07fa9
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-12 19:11:47 +0530

    Add files via upload

---
+++ 013-Return_statement.py
@@ -0,0 +1,22 @@
+# Usage of Return Statement
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+print("")
+
+
+def cube1(numbers):
+    return numbers*numbers*numbers
+
+result = cube1(4)
+print(result)
+print("")
+
+
+# Without using return sttement
+def cube3(num):
+    print(num*num*num)
+
+cube3(5)
+print("")
