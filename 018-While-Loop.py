f082d8721ff5d1207fd8999c8309be16ff7b3518
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-15 13:31:53 +0530

    Add files via upload

---
+++ 018-While-Loop.py
@@ -0,0 +1,15 @@
+# While loops are simple yet effective
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+print("loop completed")
+print("")
+
+
+j = 1
+while j + 1 <= 23:
+    print(j)
+    j += 2
+print("Another loop done")
+print("")
