c78bb939008aeb5c285c4d85b4fddd3706d49824
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-15 17:17:20 +0530

    Add files via upload

---
+++ 020-For-Loop.py
@@ -0,0 +1,25 @@
+# Usage of For Loops
+for abc in "RANDOM":
+    print(abc)
+print("")
+
+
+xyz = ["Nik", "Arr", "Sam", "Shivam"]
+for names in xyz:
+    print(names)
+print("")
+
+
+for nos in range(9, 20):
+    print(nos)
+print("")
+
+
+for aa in range(5):
+    if aa == 0:
+        print("First Iteration")
+        print("Yipee")
+
+    else:
+        print("Not first time")
+        print("naah")
