2009f9ee3cc706b218d3f7a0b2829d2e154b2d81
Shivam Singhal <singhal.shivam1771@gmail.com>
2018-10-16 00:02:05 +0530

    Add files via upload

---
+++ 025-Reading-Files.py
@@ -0,0 +1,22 @@
+# r = read
+# w = write
+# a = no changes or modifications but adding at the end (append info)
+# r+ = read and write
+
+file = open("025-Sample.txt", "r")
+print(file.readable())
+print(file.readline())
+print(file.readlines())
+file.close()
+print("")
+
+file = open("025-Sample.txt", "r")
+print(file.readlines()[2])
+file.close()
+print("")
+
+file = open("025-Sample.txt", "r")
+for abc in file.readlines():
+    print(abc)
+file.close()
+print("")
